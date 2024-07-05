223a557b3e513561534cca65f3f4c8c97728e4c2
Gaby Nieva <gaby.nieva@outlook.com>
2024-07-05 10:09:07 -0600

    Commit inicial

---
+++ main.py
@@ -0,0 +1,10 @@
+import psycopg2
+from flask import Flask, redirect, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
