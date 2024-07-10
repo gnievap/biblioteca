162624c4f1a2449039b4fb9ef07b7725b2460519
Gaby Nieva <gaby.nieva@outlook.com>
2024-07-10 12:57:50 -0600

    Autores

--- main.py
+++ main.py
@@ -25,10 +25,31 @@ def libros():
     # crear un cursor (objeto para recorrer las tablas)
     cursor = conexion.cursor()
     # ejecutar una consulta en postgres
-    cursor.execute('''SELECT * FROM libro''')
+    cursor.execute('''SELECT * FROM libros_view''')
     #recuperar la informacion
     datos = cursor.fetchall()
     #cerrar cursos y conexion a la base de datos
     cursor.close()
     conexion.close()
     return render_template('libros.html', datos=datos)
+
+@app.route('/autores')
+def autores():
+    # Conectar con la base de datos
+    conexion = psycopg2.connect(
+        database="biblioteca3a",
+        user="postgres",
+        password="gnieva",
+        host="localhost",
+        port="5432"
+    )
+    # crear un cursor (objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    # ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM autores_view''')
+    #recuperar la informacion
+    datos = cursor.fetchall()
+    #cerrar cursos y conexion a la base de datos
+    cursor.close()
+    conexion.close()
+    return render_template('autores.html', datos=datos)
