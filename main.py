24738b48ccdba3c0e36b3bf93c6d30cc57bce9a5
Gaby Nieva <gaby.nieva@outlook.com>
2024-07-16 12:00:23 -0600

    Formulario para editar país

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import psycopg2
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, request, redirect, render_template, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import PasswordField, StringField, SubmitField
@@ -93,7 +93,7 @@ def delete_pais(id_pais):
     conexion.close()
     return redirect(url_for('index'))
 
-@app.route('/update1_pais/<int:id_pais>', methods=['GET','POST'])
+@app.route('/update1_pais/<int:id_pais>', methods=['POST'])
 def update1_pais(id_pais):
     conexion = psycopg2.connect(
         database="biblioteca3a",
@@ -107,6 +107,8 @@ def update1_pais(id_pais):
     # recuperar el registro del id_pais seleccionado
     cursor.execute('''SELECT * FROM pais WHERE id_pais=%s''',
                    (id_pais,))
+    datos = cursor.fetchall()
     cursor.close()
     conexion.close()
-    return redirect(url_for('update2'))
+    return render_template('editar_pais.html', datos=datos)
+
